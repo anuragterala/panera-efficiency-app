a88f3ed9be3a16919614f777d11ad89c763620c3
anuragterala <anuragterala@gmail.com>
2024-06-24 22:45:49 +0200

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -3,21 +3,21 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true,
+    required: true
   },
   status: {
     type: String,
     enum: ['pending', 'in-progress', 'completed'],
-    default: 'pending',
+    default: 'pending'
   },
   assignedTo: {
     type: String,
-    required: true,
+    required: true
   },
   createdAt: {
     type: Date,
-    default: Date.now,
-  },
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
