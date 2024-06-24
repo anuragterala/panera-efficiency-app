81027444ed430ab1dfada83948d00f5bce916e66
anuragterala <anuragterala@gmail.com>
2024-06-24 22:44:49 +0200

    Add files via upload

---
+++ models/Task.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'in-progress', 'completed'],
+    default: 'pending',
+  },
+  assignedTo: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
