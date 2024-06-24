5ba6605ee0d14dcb9b60330fe4d2dee6a49ba559
anuragterala <anuragterala@gmail.com>
2024-06-24 22:47:29 +0200

    Update tasks.js

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,11 @@
 const Task = require('../models/Task');
 
-// Gets all of the tasks
 exports.getTasks = (req, res) => {
   Task.find()
     .then(tasks => res.json(tasks))
     .catch(err => res.status(500).json({ message: err.message }));
 };
 
-// Creates a new task
 exports.createTask = (req, res) => {
   const { title, assignedTo } = req.body;
   const newTask = new Task({ title, assignedTo });
@@ -17,7 +15,6 @@ exports.createTask = (req, res) => {
     .catch(err => res.status(400).json({ message: err.message }));
 };
 
-// Updates a task
 exports.updateTask = (req, res) => {
   const { id } = req.params;
   const { status } = req.body;
